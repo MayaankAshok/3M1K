08ac05ac4617233934eb6ce0e112daad947617bf
MadboyMassacr <maneesh.manoj@research.iiit.ac.in>
2024-01-20 21:20:34 +0530

    Adding router

--- threemonek/src/components/Login.js
+++ threemonek/src/components/Login.js
@@ -39,6 +39,7 @@ export default function Login() {
 
     return (
         <>
+            {console.log("Registration successful")}
             <div className="container my-5">
                 <div className="row justify-content-center">
                     <div className="col-md-4">
--- threemonek/src/components/Navbar.js
+++ threemonek/src/components/Navbar.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function Navbar() {
 
     return (
         <>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="light" >
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <form class="d-flex" role="search">
-                        <button type="button" class="button-54 mx-2 my-2" >Login</button>
-                        <button type="button" class="button-54 mx-2 my-2">SignUp</button>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="light" >
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Eureka</a>
+                    <form className="d-flex" role="search">
+                        <button type="button" className="button-54 mx-2 my-2">
+                        <Link className="nav-link" to="/login">Login</Link>
+                        </button>
+                        <button type="button" className="button-54 mx-2 my-2">
+                        <Link className="nav-link" to="/signup">SignUp</Link>
+                        </button>
                     </form>
                     
                 </div>
